230fce0d48d159548d5ae4b5d6a498abc18935e6
Sébastien Duplessy <sebastien@duplessy.eu>
2024-06-07 17:08:53 +0200

    fix

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -4,11 +4,11 @@ import {useState} from "react";
 function Dropdown({title, content}) {
     const [open, setOpen] = useState(true);
 
-
+    const rootClass = `dropdown${open ? "" : " dropdown--closed"}`;
     return (
-        <div className={{dropdown: true, dropdownClosed: !open}} onClick={setOpen(!open)}>
+        <div className={rootClass} onClick={() => setOpen(!open)}>
             <h1 className="title">{title}</h1>
-            <p className="content">{content}</p>
+            <p className="content">{content} {open?'open':'close'}</p>
         </div>
     );
 }
